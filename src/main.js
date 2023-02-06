7cfaacd5abd22c6307a7cb907b11b2e34e959d74
soosa <anett.soos1066@gmail.com>
2023-02-06 20:59:22 +0000

    style added, slider

--- src/main.js
+++ src/main.js
@@ -1,33 +1,52 @@
-// import patterns from './patterns';
+/// Date count down
 
-// function launchPattern(pattern) {
-//     if (typeof pattern === 'function') {
-//         tryCatch(pattern);
-//     } else if (pattern.launchQuery) {
-//         const { launchFn, launchQuery } = pattern;
-//         Array.from(document.querySelectorAll(launchQuery))
-//             .filter((elem) => elem.className.indexOf(`${launchQuery}-no-js`))
-//             .forEach((elem) => tryCatch(() => launchFn(elem)));
-//     }
-// }
+var countDownDate = new Date("August 11, 2023").getTime();
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime(); // Find the distance between now and the count down date
 
-// document.addEventListener(
-//     'DOMContentLoaded',
-//     () => {
-//         patterns.forEach(launchPattern);
-//     },
-//     false
-// );
+  var distance = countDownDate - now;
+    // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 
-var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
-var x = setInterval(function() {
-
-    // Get today's date and time
-    var now = new Date().getTime();
-  
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-    document.getElementById("demo").innerText = "hola";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "TODAY!";
+  } else {
+    document.getElementById("countdown").innerText = days + " days to go";
+  }
 });
-x();
 
+/// gallery
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	let i;
+	let slides = document.getElementsByClassName("mySlides");
+	let dots = document.getElementsByClassName("dot");
+
+	if (n > slides.length) {slideIndex = 1}    
+
+	if (n < 1) {slideIndex = slides.length}
+
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";  
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+
+	slides[slideIndex-1].style.display = "block";  
+	dots[slideIndex-1].className += " active";
+}
+ 
